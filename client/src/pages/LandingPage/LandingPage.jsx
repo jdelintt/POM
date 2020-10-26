0fc8ff4c0ce70459dac96ebcf121598ed2016698
Oliver Bigelow <oelucy@gmail.com>
2020-10-25 22:17:45 -0400

    I can upload a pdf to the db and view it

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -150,9 +150,13 @@ const LandingPage = () => {
         justify="center"
         style={{ backgroundColor: "#ffd666" }}
       >
-        <Upload {...props}>
-          <Button icon={<UploadOutlined />}>Click to Upload</Button>
-        </Upload>
+        <form action="/files/med" method="POST" encType="multipart/form-data">
+          <div className="custom-file mb-3">
+            <input type="file" name="file" id="file" className="custom-file-input"/>
+            <label htmlFor="file" className="custom-file-label">Choose File</label>
+          </div>
+          <input type="submit" value="Submit" className="btn btn-primary btn-block"/>
+        </form>
 
         <Divider>OR FILL OUT FORM HERE</Divider>
 
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const cookieParserInstance = require('cookie-parser');
 const bodyParser = require("body-parser")
 const Grid = require("gridfs-stream")
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer")
+const GridFsStorage = require("multer-gridfs-storage")
 const methodOverride = require("method-override")
 
 
@@ -18,12 +22,12 @@ app.use(bodyParser.json());
 app.use(methodOverride("_method"))
 app.use(cookieParserInstance());
 
- mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/POM", {useNewUrlParser : true, useUnifiedTopology : true}, () => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/POM", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("connected")
 })
 
 
-const conn = mongoose.createConnection(process.env.MONGODB_MED || "mongodb://localhost/MedFiles")
+const conn = mongoose.createConnection(process.env.MONGODB_URI || "mongodb://localhost/POM")
 
 let gfs;
 
@@ -32,8 +36,78 @@ conn.once("open", () => {
     gfs.collection("MedFiles")
 })
 
+const storage = new GridFsStorage({
+    url: process.env.MONGODB_URI || "mongodb://localhost/POM",
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'MedFiles'
+                }
+                resolve(fileInfo)
+            })
+        })
+    }
+})
 
-app.use("/api" , userRoutes)
+const upload = multer({ storage })
+
+app.post("/files/med",upload.single("file") , (req, res) => {
+    res.redirect("/landing")
+})
+
+app.get("/files/manyfiles", (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        if (!files || files.length === 0) {
+            res.status(400).json({
+                err : "no files exist"
+            })
+        }
+        return res.json(files)
+    })
+})
+
+app.get("/files/:filename", (req, res) => {
+    gfs.files.findOne({filename : req.params.filename}, (err, file) => {
+        if (!file ) {
+            res.status(400).json({
+                err : "no file exist"
+            })
+        }
+        return res.json(file)
+    })
+})
+
+app.get("/fileshow/:filename", (req, res) => {
+    gfs.files.findOne({filename : req.params.filename}, (err, file) => {
+        if (!file ) {
+            res.status(400).json({
+                err : "no file exist"
+            })
+        }
+        // check if pdf
+        if (file.contentType === "application/pdf") {
+            // then we will read the pdf hopefully
+            const readstream = gfs.createReadStream(file.filename)
+            readstream.pipe(res)
+        }
+        else {
+            res.status(404).json({
+                err : "not a pdf"
+            })
+        }
+    })
+})
+
+
+
+
+app.use("/api", userRoutes)
 
 
 app.listen(PORT, () => {
