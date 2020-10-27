1f12f09d6a38058f0edde07e534ed6511ce712b2
Oliver Bigelow <oelucy@gmail.com>
2020-10-27 04:42:01 -0400

    fixed the filepond component because its absolutley usless atm, and probbably forever

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Signup from "./pages/Signup/Signup"
 import PrivateRoute from "./HighOrderComponents/PrivateRoute"
 import UnPrivateRoute from "./HighOrderComponents/UnPrivateRoute"
 import PDF from "./pages/PDFTrial/PDFTrial"
-import FileComp from "./components/FilePond.jsx"
 
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -16,9 +15,7 @@ function App() {
       <UnPrivateRoute exact path = "/" component ={Login}/>
       <UnPrivateRoute path = "/signup" component ={Signup}/>
       <PrivateRoute path = "/landing"  component ={Landing}></PrivateRoute>
-      <Route path = "/pdf" component = {PDF}></Route>
-      <PrivateRoute path = "/filecomp" component = {FileComp}/>
-
+      <PrivateRoute path = "/pdf"  component ={PDF}></PrivateRoute>
 
     </Router>
 
