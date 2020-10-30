b3bc64b1810725dbaf44a531ca7e1bad3c3a9c75
Oliver Bigelow <oelucy@gmail.com>
2020-10-30 12:09:14 -0400

    fixing conflict errors

--- client/src/pages/Forms/AdvancedDirective.jsx
+++ client/src/pages/Forms/AdvancedDirective.jsx
@@ -1,15 +1,5 @@
 import React from 'react'
 import { PDFExport } from '@progress/kendo-react-pdf';
-<<<<<<< HEAD
-import { Button } from 'antd'
-
-
-import { useRef } from 'react';
-
-const ExportPDF = () => {
-
-
-=======
 import { Form, Input, Button, DatePicker, Row, Col } from 'antd';
 import './AdvancedDirective.css'
 import { useRef, useState } from 'react';
@@ -20,38 +10,12 @@ const ExportPDF = () => {
 
     const [form] = Form.useForm();
   const [formLayout, setFormLayout] = useState('horizontal');
->>>>>>> c9a415ac05c33a4f859033c9a3207f7bab353ec9
 
     const inputEl = useRef(null);
     const onButtonClick = () => {
         inputEl.current.save();
     }
 
-<<<<<<< HEAD
-    return (
-
-
-        <div>
-            <Button
-                onClick={onButtonClick}
-            >
-                Click me
-    </Button>
-
-            <PDFExport ref={inputEl} paperSize="A4">
-
-                <h1>
-                    Hello!!
-        </h1>
-                <div>
-                    <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fd17fnq9dkz9hgj.cloudfront.net%2Fbreed-uploads%2F2018%2F09%2Fdog-landing-hero-lg.jpg%3Fbust%3D1536935129%26width%3D1080&imgrefurl=https%3A%2F%2Fwww.petfinder.com%2Fdog-breeds%2F&tbnid=jPIpNCa-zhVMbM&vet=12ahUKEwi6puXPydrsAhXWAzQIHU2NARsQMygBegUIARDPAQ..i&docid=7j5L-QcDix4o2M&w=1080&h=447&q=dogs&ved=2ahUKEwi6puXPydrsAhXWAzQIHU2NARsQMygBegUIARDPAQ"></img>
-                </div>
-
-            </PDFExport>
-        </div>
-
-
-=======
     const onFormLayoutChange = ({ layout }) => {
         setFormLayout(layout);
       };
@@ -200,7 +164,6 @@ Click me
 
 </>
         
->>>>>>> c9a415ac05c33a4f859033c9a3207f7bab353ec9
     )
 }
 
