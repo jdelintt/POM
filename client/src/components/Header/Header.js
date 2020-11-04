5ed60e0404ad688c98438c29708cfa2dd0e25a2f
Jesus DeLintt <jesus.delintt@gmail.com>
2020-11-04 15:13:46 -0800

    logout fixed

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,44 +1,53 @@
 import React from 'react';
 import "../Header/Header.css";
-// import API from "./../../utils/API";
-// import { useContext, useEffect, useState } from "react";
-// import { AuthContext } from "./../../context/AuthContext";
-// import {useHistory} from 'react-router-dom'
+import {Row, Button} from 'antd'
+import API from "./../../utils/API";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "./../../context/AuthContext";
+import {useHistory} from 'react-router-dom'
 
 
 
 
-// const { user, setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
-// let history = useHistory()
-// const handlePDF = () => {
-//   history.push("/pdf")
-
-// }
-
-// const handleLogout = () => {
-//     API.logout().then(res => {
-//       console.log(res)
-//       setIsAuthenticated(false);
-//     })
-//       .then(() => {
-//         history.push("/")
-
-//       })
-//   }
 
 function Header() {
+const { setIsAuthenticated } = useContext(AuthContext);
+
+let history = useHistory()
+
+
+const handleLogout = () => {
+    API.logout().then(res => {
+      console.log(res)
+      setIsAuthenticated(false);
+    })
+      .then(() => {
+        history.push("/")
+
+      })
+  }
+
     return (
             <div className="mainHeader glass">
+
+                <Row
+                                justify="start"
+                                >
+                                   <Button
+                                   onClick={handleLogout}
+                                   >
+                                       Logout
+                                   </Button>
+                                </Row>
                 <div className="header-container">
                     <div className="row">
                         <div className="col-md-11 text-center">
                             <h1 className="POM">Peace of Mind</h1>
                                 </div>
                             <div className="col-md-1">
-                            <h2><button className="logOut" 
-                            // onClick={handleLogout}
-                            >Logout</button></h2>
+                                
+                            
                             </div>
                     </div>
                 </div>
